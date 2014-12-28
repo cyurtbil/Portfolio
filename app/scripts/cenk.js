a8f0868b68901ca99516a2d29b3e958594d127a7
cyurtbil <cenky92@gmail.com>
2014-12-27 19:26:17 -0500

    edits

--- app/scripts/cenk.js
+++ app/scripts/cenk.js
@@ -1,1 +1,20 @@
 'use strict';
+
+$(document).ready(function() {
+
+
+  // $('#musicwallet').stop(true, true).hover(function() {
+  //   $(this).css({
+  //     cursor: "pointer",
+  //     opacity: 0.5
+  //   });
+  //   $('#musicwallet a').stop(true, true).animate({opacity: 0});
+  // }, function() {
+  //   $(this).css({
+  //     opacity: 1
+  //   });
+  //   $('#musicwallet a').stop(true, true).animate({opacity: 1});
+  // });
+
+
+});
