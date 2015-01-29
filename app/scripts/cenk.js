1ce80224b5d7350fbc5515a20004932ab1f640a8
cyurtbil <cenky92@gmail.com>
2015-01-29 11:29:06 -0500

    added nimation

--- app/scripts/cenk.js
+++ app/scripts/cenk.js
@@ -15,6 +15,14 @@ $(document).ready(function() {
     $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideUp(300);
   });
 
+  $('#musicwallet2').hover(function() {
+    $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideDown(300);
+  }, function() {
+    $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideUp(300);
+  });
+
+
+  // Side Projects
   $('#algorithms').hover(function() {
     $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideDown(300);
   }, function() {
