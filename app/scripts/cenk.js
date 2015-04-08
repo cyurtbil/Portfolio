98c33ed41b6aadb38f2e4607f7294418d7e86fad
cyurtbil <cenky92@gmail.com>
2015-04-08 09:52:54 -0400

    changes

--- app/scripts/cenk.js
+++ app/scripts/cenk.js
@@ -5,24 +5,32 @@ $(document).ready(function() {
   // Projects
   // ================================================================================================
 
-  $('#onmyplate').hover(function() {
-    $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideDown(300);
-  }, function() {
-    $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideUp(300);
-  });
+  // $('#onmyplate').hover(function() {
+  //   $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideDown(300);
+  // }, function() {
+  //   $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideUp(300);
+  // });
 
-  $('#musicwallet2').hover(function() {
-    $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideDown(300);
-  }, function() {
-    $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideUp(300);
-  });
+  // $('#musicwallet2').hover(function() {
+  //   $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideDown(300);
+  // }, function() {
+  //   $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideUp(300);
+  // });
+
+  // $('#algorithms').hover(function() {
+  //   $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideDown(300);
+  // }, function() {
+  //   $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideUp(300);
+  // });
 
 
-  // Side Projects
-  // ================================================================================================
-  $('#algorithms').hover(function() {
-    $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideDown(300);
-  }, function() {
-    $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideUp(300);
+  $('#social-media-wrapper a:not([title="email"])').on('click', function(event) {
+    event.preventDefault();
+    switch($(this).attr('title')) {
+      case 'github':
+        window.open("https://github.com/cyurtbil", "_blank");
+      case 'linkedin':
+        window.open("https://www.linkedin.com/in/cenkyurtbilir", "_blank");
+    }
   });
 });
