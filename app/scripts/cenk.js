ee0ff6282886ad0330a33c1cd6c8180e32bdac9e
cyurtbil <cenky92@gmail.com>
2015-04-08 12:16:53 -0400

    edits

--- app/scripts/cenk.js
+++ app/scripts/cenk.js
@@ -2,27 +2,6 @@
 
 $(document).ready(function() {
 
-  // Projects
-  // ================================================================================================
-
-  // $('#onmyplate').hover(function() {
-  //   $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideDown(300);
-  // }, function() {
-  //   $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideUp(300);
-  // });
-
-  // $('#musicwallet2').hover(function() {
-  //   $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideDown(300);
-  // }, function() {
-  //   $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideUp(300);
-  // });
-
-  // $('#algorithms').hover(function() {
-  //   $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideDown(300);
-  // }, function() {
-  //   $(this).children('.explanation').css({cursor: 'pointer'}).stop(true, true).slideUp(300);
-  // });
-
 
   $('#social-media-wrapper a:not([title="email"])').on('click', function(event) {
     event.preventDefault();
@@ -32,5 +11,29 @@ $(document).ready(function() {
       case 'linkedin':
         window.open("https://www.linkedin.com/in/cenkyurtbilir", "_blank");
     }
+  });
+
+  $('.explanation a').on('click', function(event) {
+    event.preventDefault();
+    switch($(this).attr('title')) {
+      case 'OnMyPlate Code':
+        window.open("https://github.com/cyurtbil/OnMyPlateClient", "_blank");
+      case 'OnMyPlate Website':
+        window.open("http://onmyplatecy.com/", "_blank");
+      case 'MusicWallet Code':
+        window.open("https://github.com/cyurtbil/MusicWalletClient", "_blank");
+      case 'MusicWallet Website':
+        window.open("http://musicwalletcy.com/", "_blank");
+    }
+  });
+
+  $('#hamburger').on('click', function(event) {
+    $('body,html').animate({ scrollTop: 630 }, 700);
+  });
+
+  $('#hamburger').hover(function() {
+    $('#hamburger .line:eq(0)').toggleClass('line1');
+    $('#hamburger .line:eq(1)').toggleClass('line2');
+    $('#hamburger .line:eq(2)').toggleClass('line3');
   });
 });
